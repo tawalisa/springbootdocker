7c5a90f7282787cb7434b15d7cabf5d57adc3895
lij <tawalisa@163.com>
2017-07-13 11:19:38 +0800

    add docker and script

--- script/run.sh
+++ script/run.sh
@@ -1,5 +1,6 @@
 home_dir=/home/project
 cd $home_dir && git clone https://github.com/tawalisa/springbootdocker.git
 #cd $home_dir/springbootdocker/dockerfiles/redis && docker build -t redis . && docker run -i --name redis redis
-cd $home_dir/springbootdocker/webserver && mvn package && java -jar target/webserver-0.0.1-SNAPSHOT.jar
+#cd $home_dir/springbootdocker/webserver && mvn package && java -jar target/webserver-0.0.1-SNAPSHOT.jar
+cd $home_dir/springbootdocker/webserver && mvn package && cp target/webserver-0.0.1-SNAPSHOT.jar $home_dir/springbootdocker/dockerfiles/webservice
 cd $home_dir/springbootdocker/dockerfiles/webservice && docker build -t webservice .
